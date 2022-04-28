2992c4231a9c98a6b0db8ca3d9832d418314f88d
OmaymaK <emmablink050@gmail.com>
2022-04-28 14:21:27 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     else:
         print(f"{len(sys.argv) - 1} arguments:")
     for i, arg in enumerate(sys.argv[1:]):
-        print(f"{i + 1:>6}: {arg}")
+        print(f"{i+1:}: {arg}")
