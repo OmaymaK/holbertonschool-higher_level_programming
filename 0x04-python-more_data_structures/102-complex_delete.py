ead7b001052e60f9c9f34b93aad7419c7b16ae7b
OmaymaK <emmablink050@gmail.com>
2022-05-04 16:00:19 -0700

    task 12

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for x, y in a_dictionary.items():
+        if y == value:
+            del a_dictionary[x]
+            break
+        return a_dictionary
