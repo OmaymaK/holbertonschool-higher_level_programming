48b91358ff686c68da8930736e23c04ba3f64dbc
OmaymaK <emmablink050@gmail.com>
2022-05-17 07:11:35 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""task4"""
+
+
+class Square:
+    """class that defines a square"""
+
+    def __init__(self, size=0):
+        """inisialising"""
+        self.__size = size
+    
+    def area(self):
+        """getting size square"""
+        return self.__size ** 2
+    
+    @property
+    def size(self):
+        """do size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """return the square"""
+            self.__size = value
