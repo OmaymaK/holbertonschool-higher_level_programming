de8cde8eb9ff5db8e5585328cd1a2010359fa60c
OmaymaK <emmablink050@gmail.com>
2022-05-31 18:02:50 +0100

    adding mode read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ Reading a text file and printing it to stdout"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, mode='r', encoding="utf-8") as f:
         read_file = f.read()
         print(read_file)
