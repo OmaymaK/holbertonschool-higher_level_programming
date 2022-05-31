6efc88bca0134a8cd34e266757368faf2867b7a3
OmaymaK <emmablink050@gmail.com>
2022-05-31 18:08:49 +0100

    fixing code

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,5 +7,4 @@ def from_json_string(my_str):
     """ returns an object (Python data structure)
     represented by a JSON string"""
     res = json.loads(my_str)
-    res1 = json.dumps(res)
-    return res1
+    return res
