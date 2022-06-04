1c744fef631bbdea433138d9dfca0c9a944c36c9
OmaymaK <emmablink050@gmail.com>
2022-06-04 16:15:41 +0100

    JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Task 1"""
+import json
 
 
 class Base:
@@ -13,3 +14,11 @@ class Base:
         else:
             __class__.__nb_objects += 1
             self.id = __class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ JSON representation """
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
