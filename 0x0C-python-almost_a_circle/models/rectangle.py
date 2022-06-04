f31e553e9ede54a7a722134324cacf1bc2575270
OmaymaK <emmablink050@gmail.com>
2022-06-04 12:29:48 +0100

    rectangle's info

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,11 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def __str__(self):
+        """ rectangle's info """
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"\
+                .format(self.id, self.x, self.y, self.width, self.height)
+    
     @property
     def width(self):
         """ width property """
