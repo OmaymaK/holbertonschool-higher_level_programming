fab31e45d4a1159db88cc307f7c4b9cbf9a6202f
OmaymaK <emmablink050@gmail.com>
2022-08-01 16:04:43 +0100

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (isNaN(argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < (argv[2] + 1); i++) {
+    for (let j = 0; j < (argv[2] + 1); i++) {
+      console.log('X');
+    }
+  }
+}
