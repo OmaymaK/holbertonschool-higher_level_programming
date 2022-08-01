94785f800c9ced6662ecef750e33e9ff3a196aa7
OmaymaK <emmablink050@gmail.com>
2022-08-01 14:44:16 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { process } = require('process');
-if (process.length === 2) {
+const { argv } = require('process');
+if (argv.length === 2) {
   console.log('No argument');
-} else if (process.length === 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
