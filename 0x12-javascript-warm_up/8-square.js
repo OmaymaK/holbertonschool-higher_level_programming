515df2c580069ea860959553a843ff70e02ae061
OmaymaK <emmablink050@gmail.com>
2022-08-01 20:57:30 +0100

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,14 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const { argv } = require('process');
 
 if (isNaN(argv[2])) {
   console.log('Missing size');
 } else {
+  let str = '';
   for (let i = 0; i < (argv[2]); i++) {
-    for (let j = 0; j < (argv[2]); i++) {
-      console.log('X');
-    }
+    str += 'X';
+  }
+  for (let j = 0; j < (argv[2]); j++) {
+    console.log(str);
   }
 }
