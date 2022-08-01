6b9baa647c4bfe835c4645ea327fbaf845859ba0
OmaymaK <emmablink050@gmail.com>
2022-08-01 21:40:58 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/local/bin/node
+const { argv } = require('process');
+
+if (isNaN(argv[2]) && !argv[2]) {
+  console.log('1');
+} else {
+  const num = parseInt(argv[2]);
+  factorial(num, 1);
+}
+function factorial (num, fact) {
+  fact *= num;
+  if (num !== 2) {
+    factorial(num - 1, fact);
+  }
+  if (num === 2) {
+    console.log(fact);
+  }
+}
