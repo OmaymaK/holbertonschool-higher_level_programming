bc19a878eb6cf1a3fd6aed6336d46b58f9867f3e
OmaymaK <emmablink050@gmail.com>
2022-08-01 16:08:12 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ const { argv } = require('process');
 if (isNaN(argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < (argv[2] + 1); i++) {
-    for (let j = 0; j < (argv[2] + 1); i++) {
+  for (let i = 0; i < (argv[2]); i++) {
+    for (let j = 0; j < (argv[2]); i++) {
       console.log('X');
     }
   }
