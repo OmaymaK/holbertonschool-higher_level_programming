50c12fee2691e54f4136721791e5d451d96329cf
OmaymaK <emmablink050@gmail.com>
2022-08-01 23:07:32 +0100

    wouh

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,11 @@ const { argv } = require('process');
 
 if (!argv[3]) {
   console.log(0);
+} else {
+  argv.shift();
+  argv.shift();
+  const arr = argv.map(x => parseInt(x));
+  arr.sort((a, b) => a - b);
+  arr.pop();
+  console.log(arr[arr.length - 1]);
 }
-argv.shift();
-argv.shift();
-const arr = argv.map(x => parseInt(x));
-arr.sort((a, b) => a - b);
-arr.pop();
-console.log(arr[arr.length - 1]);
