d22c60a1267adec356b31b1b1df50d1a11d7b015
OmaymaK <emmablink050@gmail.com>
2022-08-02 16:27:21 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const arr = list;
-  arr.reverse();
+  const arr = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    arr[list.length - 1 - i] = list[i];
+  }
   return arr;
 };
