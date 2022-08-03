9b8132680026b062e746d3098db5364b7fd5ae02
Omayma Korbi <97183204+OmaymaK@users.noreply.github.com>
2022-08-03 13:33:20 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle.js');
-module.export = class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
