e00f7b1c7efa429c8b4caaf88574d0498733c260
OmaymaK <emmablink050@gmail.com>
2022-08-07 12:49:03 -0700

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ a script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
+
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     cursor = db.cursor()
