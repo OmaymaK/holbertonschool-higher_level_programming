992c632d15cdfda98d82ebebc9e88a796fd1350a
OmaymaK <emmablink050@gmail.com>
2022-08-07 12:43:52 -0700

    select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id;")
+    for t in cursor.fetchall():
+        print(t)
