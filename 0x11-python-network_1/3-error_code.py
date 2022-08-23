58bcc3cbbf6130bcb76bf2ecb916f93a4cc2e50c
OmaymaK <emmablink050@gmail.com>
2022-08-23 14:33:17 +0100

    fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         with request.urlopen(argv[1]) as url:
             print(url.read().decode('utf-8'))
     except error.HTTPError as err:
-        print("Error code {}".format(err.code))
+        print("Error code: {}".format(err.code))
