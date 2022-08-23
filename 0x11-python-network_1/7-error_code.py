73d7b790864e431c934e106b75e6829f3aa9eb4e
OmaymaK <emmablink050@gmail.com>
2022-08-23 19:32:12 +0100

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     if res.status_code >= 400:
         print("Error code: {}".format(res.status_code))
     else:
-        print(res.status_code)
+        print(res.text)
