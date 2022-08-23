32f47d38379e61a2c4fec852d05a4a9cfdbc5696
OmaymaK <emmablink050@gmail.com>
2022-08-23 20:19:25 +0100

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Python script that takes in a letter and sends a POST request."""
+
+
 from sys import argv
 import requests
 
 
 if __name__ == '__main__':
     res = requests.post('http://0.0.0.0:5000/search_user',\
-    data={'q': argv[1] if len(argv) > 1 else ""})
+        data={'q': argv[1] if len(argv) > 1 else ""})
     try:
         r = res.json()
         if r:
