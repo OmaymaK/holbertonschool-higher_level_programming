b715844a68afda1e82ac55ffd606c54045c0c836
OmaymaK <emmablink050@gmail.com>
2022-09-13 23:08:13 +0100

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const axios = require('axios');
+axios.get(process.argv[2])
+  .then(resp => {
+    let charCount = 0;
+    const films = resp.data.results;
+    const character = 18;
+    for (let i = 0; i < films.length; i++) {
+      for (const c in films[i].characters) {
+        if (films[i].characters[c].includes(character)) {
+          charCount += 1;
+        }
+      }
+    }
+    console.log(charCount);
+  });
